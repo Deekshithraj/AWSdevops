ed59ed5c5027ee3c80e334130bc7181ba1cf1cb4
Deekshithra <raghudeeksth6@gmail.com>
2025-04-22 07:00:28 +0530

    addated script

---
+++ test.sh
@@ -0,0 +1,44 @@
+mkdir deekshith
+cd naresh
+
+echo hi file1>> f1
+
+mkdir abhi
+
+cd abhi
+
+echo hello abhi >> f2
+
+yum install java -y
+
+yum install wget -y
+
+yum install unzip -y
+
+yum install vim -y
+
+yum install epel-release -y
+
+yum install ansible -y
+
+yum install git -y
+
+yum install maven -y
+
+yum install python -y
+
+yum install python-pip -y
+
+um install tree -y
+
+yum install java -y
+
+yum install nginx -y
+
+ansible --version
+
+java -version
+
+echo successfully completed
+
+
